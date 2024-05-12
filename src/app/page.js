34ddb51f927aa26704897dada013099bad6e0051
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2024-05-12 20:18:41 +0200

    'commit'

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ export default function Home() {
 	return (
 		// <main className="h-screen w-full flex items-center justify-center space-x-8 p-8">
 		// 	<img className="h-2/3" src="/images/shape.svg" alt="" />
+		// 	<img className="h-2/3" src="/images/shape.svg" alt="" />
 		<main className="h-screen w-full bg-no-repeat bg-center bg-cover flex items-center justify-center">
 			<img className="h-96" src="/images/monogram/black.svg" alt="" />
 		</main>
