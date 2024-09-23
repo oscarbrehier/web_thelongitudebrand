2c8606ddef25add23c0ef4474de2b66dace5928c
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2024-09-23 22:26:47 +0200

    soon coming

--- src/app/page.js
+++ src/app/page.js
@@ -9,18 +9,18 @@ export default function Home() {
 				<img className="sm:h-2/3 h-1/3 w-full" src="/images/landing/coming.svg" alt="" />
 			</div>
 
+			<div className="h-screen w-full absolute">
+				<img className="h-full w-full mix-blend-screen opacity-70" src="/images/landing/overlay.png" alt="" />
+			</div>
+
 			<div className="h-screen w-full absolute grid grid-cols-4 grid-rows-2">
 				<div className="sm:col-start-3 col-start-2 col-span-2 flex items-end">
 					<img className="sm:h-1/3 h-2/3 rotate-12" src="/images/landing/sticker.svg" alt="" />
 				</div>
 			</div>
-
+			
 			<div className="h-screen w-full absolute">
 				<img className="h-full w-full mix-blend-hard-light opacity-50" src="/images/landing/noise.png" alt="" />
-			</div>
-
-			<div className="h-screen w-full absolute">
-				<img className="h-full w-full mix-blend-screen opacity-70" src="/images/landing/overlay.png" alt="" />
 			</div>
 
 		</main>
