4e97d25f5c76625da20f4455e9e9fd63603fb7f0
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2024-10-29 23:12:25 +0100

    homepage

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -6,28 +6,33 @@ export default function Page({ params: { lang } }) {
 
 	return (
 
-		<PageContainer lang={lang}>
+		// <PageContainer lang={lang}>
 
-			{/* <main className="h-screen w-full p-4"> */}
-			<main className="h-screen w-full md:pt-[4.5rem] pt-[6.5rem] pb-4">
+		// 	<main className="h-screen w-full md:pt-[4.5rem] pt-[6.5rem] pb-4">
 
-				<div className="size-full flex flex-col items-center justify-center bg-neon-green space-y-8">
+		// 		<div className="size-full flex flex-col items-center justify-center bg-neon-green space-y-8">
 
-					<div className="h-auto w-auto md:p-0 sm:px-8 px-4">
+		// 			<div className="h-auto w-auto md:p-0 sm:px-8 px-4">
 
-						<img className={`svg-blur-white md:w-[40rem]`} src="/images/landing/oval-white.png" alt="" />
+		// 				<img className={`svg-blur-white md:w-[40rem]`} src="/images/landing/oval-white.png" alt="" />
 
-					</div>
+		// 			</div>
 					
-					<Link href="/shop" className="uppercase md:text-2xl text-xl font-medium font-space text-neutral-800">
-						shop now
-					</Link>
+		// 			<Link href="/shop" className="uppercase md:text-2xl text-xl font-medium font-space text-neutral-800">
+		// 				shop now
+		// 			</Link>
 
-				</div>
+		// 		</div>
 
-			</main>
+		// 	</main>
 
-		</PageContainer>
+		// </PageContainer>
+
+		<div className="h-screen w-full flex items-center justify-center">
+
+			<p className="uppercase text-2xl font-medium font-space text-neutral-800">soon coming.</p>
+
+		</div>
 
 	);
 }
